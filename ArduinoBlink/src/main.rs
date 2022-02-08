a0356e084294895c0181c51f6b2c8bb48a913d78
ichi0915 <ichi0915@gmail.com>
2022-02-07 19:16:29 -0600

    Added the blink led example and updated the readme

--- ArduinoBlink/src/main.rs
+++ ArduinoBlink/src/main.rs
@@ -1,4 +1,31 @@
+//Puts the compiler in embedded mode (it only brings lib core) and allows to write code for kernel and firmware apps
+#![no_std]
+//It dosen't expect a main function, so we can specify it manually
+#![no_main]
 
-fn main() {
-    println!("Hello, world!");
+use core::panic::PanicInfo;
+
+use arduino_hal::Peripherals;
+
+//Define the panic handler function
+//The return type "!" is equal to "Never" it means this function is not allow to return and checks it at compile time
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+	loop {}
+}
+
+//Define the entry point of the program
+#[arduino_hal::entry]
+fn main() -> ! {
+	let peripherals: Peripherals = arduino_hal::Peripherals::take().unwrap();	//Use the struct Peripherals to get access to the hardware
+	let pins: Pins = arduino_hal::pins!(peripherals);							//Use macro function to get the pins from the peripherals
+
+	let mut led = pins.d13.into_output();
+
+	led.set_high();
+
+	loop {
+		led.toggle();
+		arduino_hal::delay_ms(1000);
+	}
 }
--- README.md
+++ README.md
@@ -37,5 +37,19 @@ $ rustc --version
 rustc 1.58.1 (db9d1b20b 2022-01-20)
 ```
 
+Then we need to set the build chain of rust to a specific version that is compatible with the rust-avr runtime
 
+```bash
+rustup override set nightly-2021-01-07
+```
+
+If you don't you will get the next error
+```bash
+error[E0554]: `#![feature]` may not be used on the stable release channel
+  --> /home/ichi0915/.cargo/registry/src/github.com-1ecc6299db9ec823/avr-device-0.3.2/src/lib.rs:53:12
+   |
+53 | #![feature(llvm_asm)]
+```
+
+To tell the compiler we want to compile to the avr target we need to create the .cargo folder and inside we create a file config.toml with the configuration
 
